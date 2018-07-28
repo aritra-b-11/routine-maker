17379dca073c5466adbc2305337ef611530606da
aritra-b-11 <aritrabhattacharjee12@gmail.com>
2018-07-28 20:24:11 +0530

    init

---
+++ routine-maker.py
@@ -0,0 +1,16 @@
+# Makes Routine automatically
+# each duration is in mins
+
+# defining the duration variables
+
+class_duration = 30
+break_duration = 60
+lab_duration = 3 * class_duration
+
+total_subjects = raw_input("How many subjects? ")
+for i in total_subjects:
+    name_of_the_sibjects = raw_input("Names of these subjects? ")
+
+total_teachres = raw_input("How many teachers? ")
+for i in total_teachres:
+    name_of_the_teachers = raw_input("Names of the teachers? ")
